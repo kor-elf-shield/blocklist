a8364f98ed6b2ea602af8a1d70de97b57d7e7f5e
Leonid Nikitin <i@kor-elf.net>
2026-03-18 21:05:30 +0500

    Add examples for fetching and displaying separated IPv4 and IPv6 addresses from blocklist services

--- examples/dshield.go
+++ examples/dshield.go
@@ -21,11 +21,23 @@ func main() {
 	// limit 0 - no limit
 	limit := uint(0)
 	config := blocklist.NewConfig(limit)
+
+	// Get IPv4 and IPv6 addresses in one list
 	ips, err := blocklist.Get(url, pars, config)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(ips)
+
+	// Get IPv4 and IPv6 addresses in two lists
+	ipsV4, ipsV6, err := blocklist.GetSeparatedIPs(url, pars, config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("IPv4")
+	fmt.Println(ipsV4)
+	fmt.Println("IPv6")
+	fmt.Println(ipsV6)
 
 	/*
 		// You can also get a range of IP addresses from this service (from to)
@@ -38,10 +50,22 @@ func main() {
 		// limit 0 - no limit
 		limit := uint(0)
 		config := blocklist.NewConfigWithValidator(limit, &parser.IPRangeValidator{})
+
+		// Get IPv4 and IPv6 addresses in one list
 		ips, err := blocklist.Get(url, pars, config)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(ips)
+
+		// Get IPv4 and IPv6 addresses in two lists
+		ipsV4, ipsV6, err := blocklist.GetSeparatedIPs(url, pars, config)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("IPv4")
+		fmt.Println(ipsV4)
+		fmt.Println("IPv6")
+		fmt.Println(ipsV6)
 	*/
 }
